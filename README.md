f9ebeccce69c50e2c7517234387a81f463bc80e6
ayoub oueslati <80178145+oueslatiayoub@users.noreply.github.com>
2023-11-23 06:14:22 +0100

    integraion2,0

--- README.md
+++
@@ -1,1 +0,0 @@
-# GreenCareerHub-Back
---
+++ database.ts
@@ -0,0 +1,18 @@
+import { MongoClient, Db } from "mongodb";
+
+export class Database {
+  private static mongoClient: MongoClient;
+
+  private constructor() {}
+
+  static async initilize() {
+    this.mongoClient = await MongoClient.connect(
+        "mongodb+srv://faresmtaallah:Qp3pCGNeynB6K8Lb@cluster0.jmsw8hk.mongodb.net/?retryWrites=true&w=majority"
+            );
+  }
+
+  static getDb() {
+    return this.mongoClient.db();
+  }
+}
+//npm run dev:start:both
